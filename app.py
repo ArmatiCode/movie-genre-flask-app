878ebffdc2b34ed1fd95c94ea05a4b64f3d6018d
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2025-03-14 19:12:45 -0400

    Created feature branches, renamed empty template files.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def placeHolder():
-    return "Let's begin coding =)"
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+
+@app.route('/movies/<genre>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
