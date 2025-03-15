b6a40e99a5fa01987ebecd5d5097bd56fb8f6fb4
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2025-03-14 22:39:15 -0400

    Implemented correcting routing/loading template based on genre

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
+from movie_data import movieList
 
 app = Flask(__name__)
 
-@app.route('/movies')
-def movies():
+#Redirect homepage
+@app.route('/')
+def home():
     return render_template('movies.html')
 
+#Base route - Will display all movies and genres
+@app.route('/movies')
+def movies():
+    return render_template('movies.html', genres=movieList.values())
+
+
+#Route to display movies only in a specific genre
 @app.route('/movies/<genre>')
+def movies_via_genre(genre):
+    #If the genre key is in the movie list, load the movies
+    if genre in movieList:
+        movies_Genre = movieList[genre] #Will return the list of movies in that genre
+        return render_template('genre.html', genre=genre, movies=movies_Genre) #passes movies as list not dict
+    else:
+        return "Oops! No movies found for that genre!"
 
 
 if __name__ == '__main__':
