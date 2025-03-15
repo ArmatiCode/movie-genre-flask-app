bd50d08bdfe627f72cdfaee1d406b9d473691aaa
Antonio Cavanaugh Lillo <antonio.armati.cl@gmail.com>
2025-03-14 21:01:45 -0400

    Added a temporary dictionary (movie list) for testing

---
+++ movie_data.py
@@ -0,0 +1,6 @@
+#List of movies and their respective keys (Genre as key, movie as valu)
+movieList = {'Comedy': ['Funny1', 'Funny2', 'Funny3'],
+             'Horror': ['Scary1', 'Scary2', 'Scary3'],
+             'Romance': ['Love1', 'Love2', 'Love3'],
+             'Cult Classic': ['Cult1', 'Cult2', 'Cult3'],
+             'Documentary': ['Doc1', 'Doc2', 'Doc3']}
